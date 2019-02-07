789b52fc88c7dea3261e53fae3e7beb3c79f47c2
joshklier <joshklier@wustl.edu>
2019-02-06 20:06:26 -0600

    Create Lab 4 Group Work.R

---
+++ labs/lab4/Lab 4 Group Work.R
@@ -0,0 +1,34 @@
+## 1. Write the names of all group members.
+# Josh Klier, Michael Rimmey, Luke Ehrenstrom
+
+
+## 2. Read in the Trump Job Approval poll data.
+##    Variables are as follows:
+##    - Approve = Proportion of the respondents who approve Trump
+##    - survey_house = Survey company
+##    - end_date = Date the survey ended
+##    - sample_subpopulation = Sample type
+##    - observations = Number of observations
+##    - mode = Survey method
+
+
+
+## 3. Plot a histogram of the Trump job approval rates. 
+
+
+
+## 4. Suppose you only have the "Gallup" poll from "2/19/2017". If we know that
+##    the population variance is 0.25, what is your estimate of the sampling 
+##    distribution? 
+##    Hint: Find this poll using two conditions (survey_house, end_date)
+
+
+
+## 5. According to your answer in Q4, what are the 20th and 75th quantiles of 
+##    the distribution?
+
+
+
+## 6. Suppose a new poll suggests that the Trump approval rate is 47%. 
+##    According to your answer in Q4, what is the probability of a poll showing 
+##    support for Trump higher than this?
